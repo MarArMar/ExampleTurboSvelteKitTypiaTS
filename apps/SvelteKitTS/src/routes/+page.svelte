a6659aae75732db4d4d9bdf1444725859d0f3b8b
ArMar <mr.arthur.marchand@gmail.com>
2023-11-23 14:26:12 +0100

    u

--- apps/SvelteKitTS/src/routes/+page.svelte
+++ apps/SvelteKitTS/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   // http://127.0.0.1:5173/imported
-
   import { onMount } from "svelte";
   import {
     loadedTypia,
@@ -8,17 +7,7 @@
     exampleNotMember,
     validateMember,
   } from "./local-typia";
-  // None of these are working
-
-  // let messageLoaded = "Not loaded";
-  
-  // import { log, messageLoaded } from "../../../../packages/common/log";
-  // import { log, messageLoaded } from "../../../../packages/common/log.ts";
-  // import { log, messageLoaded } from "common/log.ts";
   import { log, messageLoaded } from "common/log";
-
-  // Same error
-  // import { messageLoadedLocalWithImport } from "./local-ts-with-ts-import";
   import { messageLoadedLocal } from "./local-ts";
 
   onMount(() => {
